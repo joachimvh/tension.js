1b06e924699b455336b2c40b7040c02b4b97cb5e
Joachim Van Herwegen <joachimvh@gmail.com>
2024-07-23 13:17:52 +0200

    Allow log:equalTo to bind values

--- src/builtins/log/equalTo.ts
+++ src/builtins/log/equalTo.ts
@@ -1,4 +1,5 @@
-import type { BuiltinCallOptions, BuiltinCheckFn, BuiltinImplementation } from '../../BuiltinUtil';
+import type { Binding } from '../../BindUtil';
+import type { BuiltinBindFn, BuiltinCallOptions, BuiltinCheckFn, BuiltinImplementation } from '../../BuiltinUtil';
 import { fancyEquals } from '../../FancyUtil';
 
 const check: BuiltinCheckFn = ({ quad }: BuiltinCallOptions): boolean | undefined => {
@@ -11,7 +12,20 @@ const check: BuiltinCheckFn = ({ quad }: BuiltinCallOptions): boolean | undefine
   return fancyEquals(quad.subject, quad.object);
 };
 
+const bind: BuiltinBindFn = ({ quad }: BuiltinCallOptions): Binding | undefined => {
+  if (quad.subject.termType === 'BlankNode' && quad.object.termType === 'BlankNode') {
+    return;
+  }
+  if (quad.subject.termType === 'BlankNode') {
+    return { [quad.subject.value]: quad.object };
+  }
+  if (quad.object.termType === 'BlankNode') {
+    return { [quad.object.value]: quad.subject };
+  }
+};
+
 export default {
   predicate: 'http://www.w3.org/2000/10/swap/log#equalTo',
   check,
+  bind,
 } satisfies BuiltinImplementation;
